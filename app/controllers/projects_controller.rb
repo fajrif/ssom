aed6fed9f539cad04fd1386d0cf9a9ca2ec0b35f
Fajri Fachriansyah <fajri82@gmail.com>
2019-04-05 21:26:36 +0700

    load more pictures

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,10 @@ class ProjectsController < ApplicationController
 
   def index
 		@categories = Category.all
+		@pictures = []
+		@categories.each_with_index do |category, index|
+			@pictures << category.pictures.page(params[:page]).per(6)
+		end
 
     respond_to do |format|
       format.html # index.html.erb
